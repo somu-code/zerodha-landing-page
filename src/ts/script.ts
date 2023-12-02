4d6c74c1dcc2d9c29bf7738098d24afbb1aef95e
Somnath Golui <code.somu@gmail.com>
2023-12-02 12:35:06 +0530

    returns calculator done

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+const inputPrincipal: HTMLInputElement = document.getElementById(
+  "principal"
+) as HTMLInputElement;
+const inputYears: HTMLInputElement = document.getElementById(
+  "years"
+) as HTMLInputElement;
+const inputInterest: HTMLInputElement = document.getElementById(
+  "interest"
+) as HTMLInputElement;
+const calculatedReturns: HTMLSpanElement = document.getElementById(
+  "show-calculated-returns"
+) as HTMLSpanElement;
+const calculateButton: HTMLButtonElement = document.getElementById(
+  "calculate-button"
+) as HTMLButtonElement;
+
+calculateButton.addEventListener("click", (event) => {
+  console.log("Clicked");
+  const capital: number = parseInt(inputPrincipal.value);
+  const years: number = parseInt(inputYears.value);
+  const interest: number = parseInt(inputInterest.value);
+  console.log(capital, years, interest);
+  const ammount: number = capital * Math.pow(1 + interest, years);
+  console.log(ammount);
+});
